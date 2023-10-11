2476f8bcdf057f79e73caacb69d90f3f9841561b
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-11 15:41:36 +0300

    logic for the counter

--- counterApp/src/App.tsx
+++ counterApp/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    setCount(count -1);
+  };
+  const reset = () => {
+    setCount( 0)
+  }
   return (
     <>
-    <h1>Counter</h1>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={increment}>Increse</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={decrease}>Decrese</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
